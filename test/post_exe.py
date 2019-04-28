f72b511c39f22d588bb084e4b9e41bb7b2072917
Dennis Porras <denporras07@gmail.com>
2019-04-27 19:45:50 -0600

    [FIX] Post-exe test

--- test/post_exe.py
+++ test/post_exe.py
@@ -6,21 +6,29 @@ path_json = input("Insert path to ilets.json: ")
 with open(path_json, "r") as file_data:
     ilet_data = json.load(file_data)
 
-test_as01 = False
-test_as02 = False
-test_as03 = False
+test_as01 = True
+test_as02 = True
+test_as03 = True
 
 
 for ilet in ilet_data:
+    op_invade = False
+    op_infect = False
+    op_retreat = False
+    is_infected = False
+    
     for operation in ilet["Operations"]:
         if operation["Operation"] == "Invade":
-            test_as01 = True
+            op_invade = True
         if operation["Operation"] == "Infect":
-            test_as01 = True
-            test_as03 = operation["Parameter"] > 0
+            op_infect = True
+            is_infected = operation["Parameter"] > 0
         if operation["Operation"] == "Retreat":
-            test_as01 = True
-            test_as02 = True
+            op_retreat = True
+
+    test_as01 = test_as01 and (op_invade and op_infect and op_retreat)
+    test_as02 = test_as02 and op_retreat
+    test_as03 = test_as03 and is_infected
 
 
 print("Test[AS-01]:", ("PASS" if test_as01 else "FAIL"))
